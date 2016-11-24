9c5bf1f74cb69581423c57fce01f7948ee42aa00
Kate Preston <ktweeden@googlemail.com>
2016-11-24 20:40:48 +0000

    refactored issue.js

--- server/issue.js
+++ server/issue.js
@@ -1,44 +1,34 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-var issueSchema = mongoose.Schema({
+const issueSchema = mongoose.Schema({
   title: {type: String, unique: true},
   description: String,
   relatedTags: [{type: mongoose.Schema.Types.ObjectId, ref: 'Issue'}]
 });
 
-var Issue = mongoose.model('Issue', issueSchema);
+const Issue = mongoose.model('Issue', issueSchema);
 
 
 /**
  * Searches by title whether issue and returns boolean
  */
-var checkIssueExists = function(issueTitle, onCheck) {
-  console.log('checking ' + issueTitle);
-  Issue.find({title:issueTitle}, function (error, docs) {
-    console.log(docs);
-    if (error){
-      onCheck(error);
-    }
-    else if(docs.length){
-       onCheck(null, true);
-    }
-    else {
-      onCheck(null, false);
-    }
-  });
+const checkIssueExists = function(issueTitle, onCheck) {
+  Issue.find({title:issueTitle}
+  .then(docs => onCheck(null, !!docs.length))
+  .catch(onCheck)
 }
 
 /**
  * Adds issue to the database. Passes issue object to callback.
  */
-var addIssueToDb = function (issueObject, onSave) {
-  var issue = new Issue({title: issueObject.title, description:issueObject.description, relatedTags: issueObject.relatedTags});
-  issue.save(function (error, issue, numAffected) {
-    if (error) {
-      onSave(error);
-    }
-    onSave(null, issue);
-  });
+const addIssueToDb = function (issueObject, onSave) {
+  new Issue({
+    title: issueObject.title,
+    description:issueObject.description,
+    relatedTags: issueObject.relatedTags
+  }).save(issue)
+  .then(doc => onSave(null, doc))
+  .catch(onSave)
 }
 
 module.exports = {
