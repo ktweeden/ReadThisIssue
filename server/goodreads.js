09556a2fffb1b0b9a61162ec73d519a2cce3b03f
Kate Preston <ktweeden@googlemail.com>
2016-12-17 10:25:22 +0000

    sexified goodreads.js

--- server/goodreads.js
+++ server/goodreads.js
@@ -31,22 +31,18 @@ const searchGoodreads = function (bookIdentifyer, onFound) {
 
    //Send query to goodreads
    request.get(searchUrl)
-   .then(body => {
-     //parse goodreads xml search result to JSON string
-     xml2js.parseString(body, function (error, parsed){
-       if (!!error){
-         return onFound(error)
-       }
-       //check if there are any results matching search
-       if (parsed.GoodreadsResponse.search[0]['total-results'][0] === '0') {
-         return onFound(null, [])
-       }
-       //create new, cleaner string of relevant result info and pass to callback
-       onFound(null, parseWorks(parsed.GoodreadsResponse.search[0].results[0].work))
-     })
+   .then(parseXmlPromise)
+   .then(parsed => {
+     //check if there are any results matching search
+     if (parsed.GoodreadsResponse.search[0]['total-results'][0] === '0') {
+       return onFound(null, [])
+     }
+     //create new, cleaner string of relevant result info and pass to callback
+     onFound(null, parseWorks(parsed.GoodreadsResponse.search[0].results[0].work))
    })
    .catch(onFound)
 }
+
 
 /*
  * Returns book info from Goodreads given Goodreads ID
@@ -66,38 +62,34 @@ function getBookByGoodreadsID(goodreadsBookId, onFound) {
 /**
  * Create cleaned up array of 'work' objects from goodreads search response xml
  */
-var parseWorks = function (arrayOfWorks) {
-  var listOfWorks =[];
-  for (var work of arrayOfWorks) {
-    var workObject = {};
-    workObject.workID = work.id[0]['_'];
-    workObject.bookID = work.best_book[0].id[0]['_'];
-    workObject.publicationYear = work.original_publication_year[0]['_'];
-    workObject.title = work.best_book[0].title[0];
-    workObject.author = work.best_book[0].author[0].name[0];
-    workObject.cover = work.best_book[0].image_url[0];
-    listOfWorks.push(workObject);
-  }
-  return listOfWorks;
+function parseWorks(arrayOfWorks) {
+
+  return arrayOfWorks.map(work => ({
+    workID: work.id[0]['_'],
+    bookID: work.best_book[0].id[0]['_'],
+    publicationYear: work.original_publication_year[0]['_'],
+    title: work.best_book[0].title[0],
+    author: work.best_book[0].author[0].name[0],
+    cover: work.best_book[0].image_url[0]
+  })
 }
 
 /**
  * Create book object from goodreads book by ID response xml
  */
-var parseBook = function (bookResponse) {
-  var response = bookResponse.GoodreadsResponse.book[0];
-  var book = {};
-  book.title = response.title[0];
-  book.author = response.authors[0].author[0].name[0];
-  book.publicationYear = response.work[0].original_publication_year[0]['_'];
-  book.description = response.description[0];
-  book.cover = response.image_url[0];
-  book.bookID = response.id[0];
-  return book;
+function parseBook(bookResponse) {
+  const response = bookResponse.GoodreadsResponse.book[0]
+  return {
+    title: response.title[0],
+    author: response.authors[0].author[0].name[0],
+    publicationYear: response.work[0].original_publication_year[0]['_'],
+    description: response.description[0],
+    cover: response.image_url[0],
+    bookID: response.id[0]
+  }
 }
 
 module.exports = {
   searchGoodreads: searchGoodreads,
   getBookByGoodreadsID: getBookByGoodreadsID,
-  error:error
 }
